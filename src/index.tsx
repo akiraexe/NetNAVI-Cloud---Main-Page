955315cca52e584a492ac31505480c1361b14835
Marco <marcoa.mendozar@gmail.com>
2026-06-17 23:57:34 -0600

    Second changed wit Gemini

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// TypeScript may complain about side-effect CSS imports when no declaration exists.
+// @ts-ignore: allow importing CSS for side effects
 import './index.css';
 
 const rootElement = document.getElementById('root');
